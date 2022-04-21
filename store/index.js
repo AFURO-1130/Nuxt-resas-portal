ee415b2b11c366e69f92b4858e414f132bbfb69f
AFURO-1130 <softtennis6161130@icloud.com>
2022-04-21 11:44:26 +0900

    storeからcomponentsに値を流すことはできた

--- store/index.js
+++ store/index.js
@@ -9,14 +9,5 @@ export const mutations = {
 }
 
 export const actions = {
-  // async getPref({ commit, $config }) {
-  //   await this.$axios
-  //     .$get('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
-  //       headers: { 'X-API-KEY': '$config.apiKey' },
-  //     })
-  //     .then(async (res) => {
-  //       console.log('res', res)
-  //       await commit('setPref',res)
-  //     })
-  // },
+
 }
